cdbcd74f65041a63ae114fee9b292f4fcf3020fa
Kensaa <kensadevs@gmail.com>
2023-10-10 16:55:02 +0200

    added a total time played bar graph

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,11 +1,13 @@
 import Week from './pages/Week'
 import SingleDay from './pages/SingleDay'
+import TotalTime from './pages/TotalTime'
 
 export default function App() {
     return (
         <div className='w-100 h-100'>
             <SingleDay />
             <Week />
+            <TotalTime />
         </div>
     )
 }
